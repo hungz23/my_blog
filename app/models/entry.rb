edab3832a5e574345656a9fbd231e23e375bd1dd
hungz23 <hungtranbuddha@gmail.com>
2016-07-18 13:53:58 +0700

    stupid

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -3,7 +3,7 @@ class Entry < ActiveRecord::Base
   belongs_to :user
   default_scope -> { order(created_at: :desc)}
   validates :user_id, presence: true
-  validates :content, presence: true, length: { maximum: 140}
+  validates :content, presence: true, length: { maximum: 140000}
   def index
   	@entry = Entry.all
   end
